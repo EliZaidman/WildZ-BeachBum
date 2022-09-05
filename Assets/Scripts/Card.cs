9eb641c3647d046d681f39fb07310131366df8b5
Eli Zaidman <Rxelik5@gmail.com>
2022-09-05 09:44:00 +0300

    GL HF

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+
+    static string Color;
+    static int Num;
+
+
+    enum C_Color
+    {
+        Red,
+        Blue,
+        Green,
+        Yellow,
+
+    }
+}
---
+++ Assets/Scripts/CardManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardManager : MonoBehaviour
+{
+
+    public List<Card> RedCards;
+    public List<Card> BlueCards;
+    public List<Card> YellowCards;
+    public List<Card> GreenCards;
+
+
+}
